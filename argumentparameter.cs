ea94ee57b5c308a6f38c07c1beb6c964c242156e
“Nabin <pona_bca2079@lict.edu.np>
2025-03-12 18:01:54 +0545

    Argumtentparameter

---
+++ argumentparameter.cs
@@ -0,0 +1,13 @@
+using Systme;
+class HEllo{
+    public static void Main()
+    {
+        int a,b;
+        System.Console.WriteLine("EThe sum of "+a+" and "+b+" is "+sum(a,b));
+    }
+    public static sum(int a , int b)
+    {
+        int sum=a+b;
+        return sum;
+    }
+}
