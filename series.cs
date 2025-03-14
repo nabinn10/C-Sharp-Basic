efe4c89e7d1317dc7984c25cbcedd4ee407560ff
“Nabin <pona_bca2079@lict.edu.np>
2025-03-14 19:15:58 +0545

    Series

---
+++ series.cs
@@ -0,0 +1,16 @@
+using System;
+class Series
+{
+    public static void Main()
+    {
+        int i,j;
+        for(i=1;i<=5;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
