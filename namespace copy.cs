eb7238b4aa359198d11b7a448e233e7ca7df0354
“Nabin <pona_bca2079@lict.edu.np>
2025-04-12 19:26:00 +0545

    Some Update

---
+++ namespace copy.cs
@@ -0,0 +1,11 @@
+using System;
+namespace MyNamespace
+{
+    public class Myclass
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Hello from MyNamespace");
+        }
+    }
+}
