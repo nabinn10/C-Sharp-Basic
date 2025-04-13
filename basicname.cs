44df2471296a36da71c64dcd4826fec87f516f04
Nabin  Poudel <pona_bca2079@lict.edu.np>
2025-04-13 16:53:54 +0545

    Simple program

---
+++ basicname.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your name:");
+        string name = Console.ReadLine();  // Read input from the user
+
+        Console.WriteLine("Hello, " + name + "! Welcome to C# programming.");
+    }
+}
