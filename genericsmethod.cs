dbdf2cab8944ba4d72ef6460ae4829d04b34f2b9
“Nabin <pona_bca2079@lict.edu.np>
2025-04-29 14:05:03 +0545

    Generics method

---
+++ genericsmethod.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System;   
+
+class Program{
+
+    static void swap<t>(ref t a, ref t b){
+        t temp = a;
+        a = b;
+        b = temp;
+    }
+    static void Main(){
+        int a = 10, b = 20;
+        Console.WriteLine("Before swap: a "+a+" b = "+b);
+        swap<int>(ref a, ref b);
+        Console.WriteLine("Swapped values: a = " + a + ", b = " + b);
+
+    }
+}
