4079040650f9573c5054d5cc0044fe5333a86000
“Nabin <pona_bca2079@lict.edu.np>
2025-05-04 13:41:55 +0545

    SOme Update

--- genericsmethod.cs
+++ genericsmethod.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using System;   
-
 class Program{
 
     static void swap<t>(ref t a, ref t b){
@@ -8,7 +7,7 @@ class Program{
         a = b;
         b = temp;
     }
-    static void Main(){
+    static void Main(){r
         int a = 10, b = 20;
         Console.WriteLine("Before swap: a "+a+" b = "+b);
         swap<int>(ref a, ref b);
